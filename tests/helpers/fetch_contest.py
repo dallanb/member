b621d6c92cac34b9931caa38a2a8cd80e8bcac3d
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-02-21 20:25:20 -0800

    add tests for events accounts and contests

--- tests/helpers/fetch_contest.py
+++ tests/helpers/fetch_contest.py
@@ -23,12 +23,12 @@ def fetch_contest(self, uuid):
             }
         if pytest.other_member is not None and not inspect(pytest.other_member).detached:
             contest['participants'][str(pytest.other_member.uuid)] = {
-                                                                         'score': 1,
-                                                                         'status': 'completed',
-                                                                         'strokes': '74',
-                                                                         'member_uuid': str(pytest.other_member.uuid),
-                                                                         'display_name': pytest.other_display_name
-                                                                     },
+                'score': 1,
+                'status': 'completed',
+                'strokes': '74',
+                'member_uuid': str(pytest.other_member.uuid),
+                'display_name': pytest.other_display_name
+            }
         return contest
     else:
         return None
