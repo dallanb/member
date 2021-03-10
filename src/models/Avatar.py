246e573b9a63b0252ee5159946f0e3a006f9a814
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-03-10 11:54:06 -0800

    fix delele api call

--- src/models/Avatar.py
+++ src/models/Avatar.py
@@ -6,7 +6,7 @@ class Avatar(db.Model, BaseMixin):
     s3_filename = db.Column(db.String, nullable=False)
 
     # Relationship
-    member = db.relationship("Member", back_populates="avatar", uselist=False)
+    member = db.relationship("Member", back_populates="avatar", uselist=False, lazy="joined")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
