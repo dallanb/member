f397ee43ad38a6979c1d13bf18bb28291f6bae95
Dallan Bhatti <dallan.bhatti@techtapir.com>
2021-03-13 17:46:41 -0800

    add some important info to the wallet and stat notifications

--- src/notifications/stat_updated/schema.py
+++ src/notifications/stat_updated/schema.py
@@ -1,7 +1,15 @@
-from marshmallow import Schema
+from marshmallow import Schema, pre_dump
 from webargs import fields
 
 
 class StatUpdatedSchema(Schema):
+    league_uuid = fields.UUID(missing=None, attribute='member.league_uuid')
+    user_uuid = fields.UUID(attribute='member.user_uuid')
     member_uuid = fields.UUID(attribute='stat.member_uuid')
     uuid = fields.UUID(attribute='stat.uuid')
+
+    @pre_dump
+    def prepare(self, data, **kwargs):
+        stat = data['stat']
+        data['member'] = stat.member
+        return data
